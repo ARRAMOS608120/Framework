80adc2135e682598f6ea763c59b073350f452f36
ARRAMOS608120 <89816526+ARRAMOS608120@users.noreply.github.com>
2022-02-10 11:01:19 -0300

    Update controlador.js

--- controlador/controlador.js
+++ controlador/controlador.js
@@ -154,42 +154,6 @@ const calculoRandom = async ctx =>  {
     console.log(req.user.username)
   }
 
-/*const {graphqlHTTP} = require ('express-graphql');
-const {buildSchema}= require ('graphql');
-
-const schema = buildSchema(`
-  input ProductoInput {
-    title: String,
-    price: Int,
-    thumbnail: String
-  }
-  type Producto {
-    id: ID!
-    title: String,
-    price: Int,
-    thumbnail: String
-  }
-  type Query {
-    getProductos: [Producto],
-  }
-  type Mutation {
-    createProducto(producto:ProductoInput): Producto
-  }
-`);
-
-class GraphQLController {
-  constructor() {
-      return graphqlHTTP({
-          schema: schema,
-          rootValue: {
-              getProductos: listadoProductos,
-              createProducto: guardarProducto
-          },
-          graphiql: true,
-      })
-  }
-}*/
-
 const cargarProducto  = async ctx => {
     const productJson = ctx.request.body
     console.log(productJson)
